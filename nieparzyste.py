a876d8873e883c18f4f655041cd06294125dd721
Krzysztof Zelazny <headsho7@o2.pl>
2024-01-22 20:40:20 +0100

    Drobne poprawki, dodatkowy warunek

--- nieparzyste.py
+++ nieparzyste.py
@@ -2,11 +2,11 @@ licz=0
 suma=0
 while licz < 3:
    liczba=(int(input("Dawaj liczbę: ")))
-   if (liczba%2==0):
+   if (liczba%2==0) and (liczba>0):
       print(liczba,"+",suma,"=",liczba+suma)
       suma = liczba + suma;
       licz+=1
    else:
-      print("Parzystą mordo.")
+      print("Parzystą dodatnią mordo.")
       continue
 print("Suma to",suma)
