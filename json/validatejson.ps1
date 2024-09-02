f11a9132ef0c8390a33b5f0bad3fef968ebbb8c0
Krzysztof Bigelmajer <headsho7@o2.pl>
2024-09-02 14:50:21 +0200

    Removed a redundant else statement

--- json/validatejson.ps1
+++ json/validatejson.ps1
@@ -10,7 +10,4 @@ $result = $jsonobj | Test-Json -SchemaFile $schemaFile
 if ($result){
     #happy path, data matches the schema
     Write-Output("All good, a perfect match!")
-} else {
-    #In case of any mismatches the data will be printed here
-    $result
 }
