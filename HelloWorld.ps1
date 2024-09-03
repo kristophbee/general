8a9101eac09e3872f324418b6d7178bf5f1ac225
Krzysztof Zelazny <headsho7@o2.pl>
2024-09-03 20:10:51 +0200

    Added new script during training

---
+++ HelloWorld.ps1
@@ -0,0 +1,5 @@
+Write-Output 'Hello Macro$oft!'
+$morda = Read-Host -Prompt "Mordo weź się wpisz"
+Write-Output "Gratki $morda! Właśnie przegrałeś grę!"
+$today = Get-Date
+Write-Output "Dzisiaj jest $today"
