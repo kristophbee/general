7f97ec4c7f86d4c5d9a87b9c5fd8a5655a2897df
Krzysztof Zelazny <headsho7@o2.pl>
2025-09-17 17:30:38 +0200

    Added logfile and debug file

--- syncer/sync.py
+++ syncer/sync.py
@@ -5,77 +5,70 @@ import hashlib
 import shutil
 import time
 
-# 1. source path
-# 2. replica path
-# 3. interval time
-# 4. number of intervals to keep
-# 5. path to log file
-#TODO add debug logs
-logging.basicConfig(filename="debug.log", level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 # Function to compute the checksum of a file
 def compute_checksum(file_path):
-    logging.debug(f"Computing checksum for {file_path}")
+    logging.debug("Computing checksum for"+str(file_path))
     hash_md5 = hashlib.md5()
     with open(file_path, "rb") as f:
         for chunk in iter(lambda: f.read(4096), b""):
             hash_md5.update(chunk)
-            logging.debug(f"Read chunk of size {len(chunk)}")
+            logging.debug("Read chunk of size "+str(len(chunk)))
     return hash_md5.hexdigest()
 
 # Scan the source directory and build a dictionary of files with their checksums
 def scan_directory(path):
-    logging.debug(f"Scanning directory {path}")
+    logging.debug("Scanning directory "+str(path))
     file_dict = {}
     for root, dirs, files in os.walk(path):
         for file in files:
             full_path = os.path.join(root, file)
-            logging.debug(f"Found file {full_path}")
+            logging.debug("Found file "+str(full_path))
             rel_path = os.path.relpath(full_path, path)
-            logging.debug(f"Relative path: {rel_path}")
+            logging.debug("Relative path: "+str(rel_path))
             checksum = compute_checksum(full_path)
-            logging.debug(f"Checksum for {rel_path}: {checksum}")
+            logging.debug("Checksum for "+str(rel_path)+": "+str(checksum))
             file_dict[rel_path] = checksum
     return file_dict
 
 # Sync the source directory to the replica directory
 def sync_directories(source, replica):
-    logging.debug(f"Syncing from {source} to {replica}")
+    logging.debug("Syncing from "+str(source)+" to "+str(replica))
     source_files = scan_directory(source)
-    logging.debug(f"Source files: {source_files}")
+    logging.debug("Source files: "+str(source_files))
     replica_files = scan_directory(replica)
-    logging.debug(f"Replica files: {replica_files}")
+    logging.debug("Replica files: "+str(replica_files))
 
     # Copy new and modified files from source to replica
     for rel_path, checksum in source_files.items():
         source_file = os.path.join(source, rel_path)
-        logging.debug(f"Processing file {source_file}")
+        logging.debug("Processing file "+str(source_file))
         replica_file = os.path.join(replica, rel_path)
-        logging.debug(f"Corresponding replica file {replica_file}")
+        logging.debug("Corresponding replica file "+str(replica_file))
 
         if rel_path not in replica_files or replica_files[rel_path] != checksum:
-            logging.debug(f"File {rel_path} is new or modified")
+            logging.debug("File "+str(rel_path)+" is new or modified")
             os.makedirs(os.path.dirname(replica_file), exist_ok=True)
-            logging.debug(f"Copying file to {replica_file}")
+            logging.debug("Copying file to "+str(replica_file))
             shutil.copy2(source_file, replica_file)
-            logging.info(f"Copied/Updated: {rel_path}")
+            logging.info("Copied/Updated: "+str(rel_path))
 
     # Remove files from replica that are not in source
     for rel_path in replica_files.keys():
-        logging.debug(f"Checking if {rel_path} needs to be removed")
+        logging.debug("Checking if "+str(rel_path)+" needs to be removed")
         if rel_path not in source_files:
             replica_file = os.path.join(replica, rel_path)
             os.remove(replica_file)
-            logging.info(f"Removed: {rel_path}")
+            logging.info("Removed: "+str(rel_path))
             # Remove empty directories
             dir_path = os.path.dirname(replica_file)
             while dir_path != replica and not os.listdir(dir_path):
                 os.rmdir(dir_path)
                 dir_path = os.path.dirname(dir_path)
-                logging.info(f"Removed empty directory: {dir_path}")
+                logging.info("Removed empty directory: "+str(dir_path))
 
 #main call
 def main(original=sys.argv[1], replica=sys.argv[2], interval=int(sys.argv[3]), num_intervals=int(sys.argv[4]), logfile=sys.argv[5]):
-    logging.debug(f"Arguments received - original: {original}, replica: {replica}, interval: {interval}, num_intervals: {num_intervals}, logfile: {logfile}")
+    logging.debug("Arguments received - original: "+str(original)+", replica: "+str(replica)+", interval: "+str(interval)+", num_intervals: "+str(num_intervals)+", logfile: "+str(logfile))
     logging.basicConfig(filename=logfile, level=logging.INFO, format='%(asctime)s - %(message)s')
     logging.info("Starting synchronization process")
     try:
